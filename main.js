ab419b4f48b52f67cacc480bf5971d485cd3fbaa
Tom <tom@Toms-iMac.local>
2018-03-31 22:14:46 +0200

    Sons

--- main.js
+++ main.js
@@ -1,19 +1,33 @@
 var player;
-var enemies = new Array(new Forme(),new Forme());
+var enemies = new Array();
+var sSuccess, sChange, sMusic, sDeath;
 
+function preload() {
+  sSuccess = loadSound("res/success.wav");
+  sChange = loadSound("res/change.wav");
+  sDeath = loadSound("res/death.wav");
+  sMusic = loadSound("res/music.wav");
+}
 
 function setup() {
-  var canvasW = 800;
-  var canvasH = 600;
+  createCanvas(800, 800);
+  enemies.push(new Forme(0, 0));
+  enemies.push(new Forme(0, 0));
+  player = new Forme(width/2,height/2);
 
-  createCanvas(canvasW, canvasH);
-  player = new Forme(canvasW/2,canvasH/2);
+  sMusic.amp(0.5);
+  //sMusic.loop();
 
 }
 function keyTyped(){
     if(key === ' '){
         player.forme += 1;
         player.forme %= 3;
+
+        sChange.amp(random(0.5, 0.8));
+        sChange.rate(random(0.9, 1.1));
+        sChange.play();
+
     }
 }
 
@@ -21,7 +35,7 @@ function keyTyped(){
 function draw() {
     background(51, 110, 123);
     player.draw();
-    for (enemy in enemies){
+    for (enemy of enemies){
         enemy.draw();
     }
 
