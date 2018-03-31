177a84f266df16f87eae52accd8255fb8be290c6
GuiguiV <guillaume.v.gv@gmail.com>
2018-03-31 21:38:36 +0200

    input

--- main.js
+++ main.js
@@ -2,11 +2,21 @@ var f;
 
 function setup() {
   createCanvas(800, 600);
-  f = new Forme();
+  player = new Forme();
+
 }
+function keyTyped(){
+    if(key === ' '){
+        player.forme += 1;
+        player.forme %= 3;
+        print(player.forme);
+    }
+
+}
+
 
 function draw() {
     background(51, 110, 123);
-    f.draw();
+    player.draw();
 
 }
