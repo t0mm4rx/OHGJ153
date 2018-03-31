dad33dd9a39dbeeb783921ec0d89e577c17216af
GuiguiV <guillaume.v.gv@gmail.com>
2018-03-31 22:03:09 +0200

    setup

--- main.js
+++ main.js
@@ -1,22 +1,28 @@
-var f;
+var player;
+var enemies = new Array(new Forme(),new Forme());
+
 
 function setup() {
-  createCanvas(800, 600);
-  player = new Forme();
+  var canvasW = 800;
+  var canvasH = 600;
+
+  createCanvas(canvasW, canvasH);
+  player = new Forme(canvasW/2,canvasH/2);
 
 }
 function keyTyped(){
     if(key === ' '){
         player.forme += 1;
         player.forme %= 3;
-        print(player.forme);
     }
-
 }
 
 
 function draw() {
     background(51, 110, 123);
     player.draw();
+    for (enemy in enemies){
+        enemy.draw();
+    }
 
 }
