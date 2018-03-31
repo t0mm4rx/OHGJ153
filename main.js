82252c55db0b33939afe20783d394c292bc84155
Tom <tom@Toms-iMac.local>
2018-03-31 22:05:20 +0200

    Merge

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 var f;
+var soundChange, soundDeath, soundSuccess, music;
+
+function preload() {
+  soundChange = loadSound('res/change.wav');
+  soundDeath = loadSound('res/death.wav');
+  soundSuccess = loadSound('res/success.wav');
+  music = loadSound('res/music.wav');
+}
 
 function setup() {
   createCanvas(800, 600);
   player = new Forme();
+  music.amp(0.2);
+  //music.loop();
 
 }
 function keyTyped(){
@@ -10,6 +20,9 @@ function keyTyped(){
         player.forme += 1;
         player.forme %= 3;
         print(player.forme);
+        soundChange.amp(random(0.5, 0.8));
+        soundChange.rate(random(0.9, 1.1));
+        soundChange.play();
     }
 
 }
