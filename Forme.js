f3e4c4f21e43c114b738a6beb3f18a07f954a380
Tom <tom@Toms-iMac.local>
2018-03-31 21:15:50 +0200

    Classe forme

---
+++ Forme.js
@@ -0,0 +1,21 @@
+function Forme() {
+
+  this.size = 50;
+
+  this.pos = createVector(width / 2, height / 2);
+  //this.pos = {x: width / 2, y: height / 2};
+
+  this.forme = 2; // 0 = carré, 1 = triangle, 2 = rond
+
+  this.draw = function() {
+    fill(0);
+    if (this.forme == 0) {
+      rect(this.pos.x, this.pos.y, this.size, this.size);
+    } else if (this.forme == 1) {
+      triangle(this.pos.x - this.size / 2, this.pos.y - this.size / 2, this.pos.x, this.pos.y + this.size / 2, this.pos.x + this.size / 2, this.pos.y - this.size / 2);
+    } else if (this.forme == 2) {
+      ellipse(this.pos.x, this.pos.y, this.size, this.size);
+    }
+  }
+
+}
--- main.js
+++ main.js
@@ -1,7 +1,12 @@
+var f;
+
 function setup() {
   createCanvas(800, 600);
+  f = new Forme();
 }
 
 function draw() {
     background(51, 110, 123);
+    f.draw();
+
 }
