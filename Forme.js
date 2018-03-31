9db496c5e849fb7114d49af902a3a59843f538fa
Tom <tom@Toms-iMac.local>
2018-03-31 22:38:21 +0200

    Movement

--- Forme.js
+++ Forme.js
@@ -1,11 +1,13 @@
-function Forme(x,y) {
+function Forme(x,y,f,dir) {
 
   this.size = 50;
 
   this.pos = createVector(x,y);
   //this.pos = {x: width / 2, y: height / 2};
 
-  this.forme = 2; // 0 = carré, 1 = triangle, 2 = rond
+  this.forme = f; // 0 = carré, 1 = triangle, 2 = rond
+  this.dir = dir; // -1 = player; 0 = haut, 1 = bas, 2 = gauche, 3 = droite
+  this.speed = 3;
 
   this.draw = function() {
     fill(0);
@@ -17,6 +19,20 @@ function Forme(x,y) {
     } else if (this.forme == 2) {
       ellipse(this.pos.x, this.pos.y, this.size, this.size);
     }
+
+    if (this.dir == 0) {
+        this.pos.y += this.speed;
+    }
+    if (this.dir == 1) {
+this.pos.y -= this.speed;
+    }
+    if (this.dir == 2) {
+this.pos.x += this.speed;
+    }
+    if (this.dir == 3) {
+this.pos.x -= this.speed;
+    }
+
   }
 
 }
--- main.js
+++ main.js
@@ -11,9 +11,9 @@ function preload() {
 
 function setup() {
   createCanvas(800, 800);
-  enemies.push(new Forme(0, 0));
-  enemies.push(new Forme(0, 0));
-  player = new Forme(width/2,height/2);
+  /*enemies.push(new Forme(0, 0));
+  enemies.push(new Forme(0, 0));*/
+  player = new Forme(width/2,height/2, 0, -1);
 
   sMusic.amp(0.5);
   //sMusic.loop();
@@ -37,6 +37,25 @@ function draw() {
     player.draw();
     for (enemy of enemies){
         enemy.draw();
+    }
+
+    if (frameCount % 60 == 0) {
+
+      let pos = random(0, 4);
+      if (pos <= 1) {
+          // Haut
+          enemies.push(new Forme(width / 2, 50, floor(random(0, 3)), 0));
+      } else if (pos <= 2) {
+        // Bas
+        enemies.push(new Forme(width / 2, height - 50, floor(random(0, 3)), 1));
+      } else if (pos <= 3) {
+        // Gauche
+        enemies.push(new Forme(50, height / 2, floor(random(0, 3)), 2));
+      } else {
+        // Droite
+        enemies.push(new Forme(width - 50, height / 2, floor(random(0, 3)), 3));
+      }
+
     }
 
 }
